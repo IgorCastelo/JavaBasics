262ca16f4131d1e2663701630c2528d1b96f504d
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-06 01:33:21 -0300

    Foi adicionado o exercicio sobre a estrutura switch

---
+++ src/com/igorcastelo/javabasico/intrucaoswitch/Main.java
@@ -0,0 +1,27 @@
+package com.igorcastelo.javabasico.intrucaoswitch;
+
+public class Main {
+
+	public static void main(string[] args) {
+		//istrução switch(exemplo de valida��o de termos do usu�rio).
+	     /*  char resposta = 's';
+	       
+	     
+	       switch(resposta){
+	           case 's':
+	           case 's':    
+	               system.out.println("voce aceitou os termos");
+	               break;
+	           case 'n':
+	           case 'n':    
+	               system.out.println("voce nao ceitou os termos");
+	               break;
+	           default:
+	                system.out.println("error");
+	                
+	       }
+	   */ 
+
+	}
+
+}
