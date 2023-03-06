953455a0733611e51d7b2741e4cc1a7320d6c89b
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-06 01:55:06 -0300

    Added exercise explaining 'ForEach' structure

---
+++ src/com/igorcastelo/javabasico/structureforeach/Main.java
@@ -0,0 +1,12 @@
+package com.igorcastelo.javabasico.structureforeach;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] sports = {"basketBall", "Soccer", "JiuJitsu", "SkateBoard"};
+		for (String i : sports) {
+		  System.out.println(i);
+		}
+	}
+
+}
