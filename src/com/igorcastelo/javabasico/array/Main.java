bb54a22a814b88ef6f8dd91a150c9aa0d3d3b77a
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-06 21:21:20 -0300

    Added exercise explaining 'array' structure

---
+++ src/com/igorcastelo/javabasico/array/Main.java
@@ -0,0 +1,19 @@
+package com.igorcastelo.javabasico.array;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double[] temperaturas = new double [65];
+	       
+	       
+	       for(int i = 0; i<=temperaturas.length;i++){
+	       temperaturas[i]= i++; }
+	       
+	        for(int i=0;i<=temperaturas.length;i++){
+	       System.out.println("O valor da temperatura de hj, dia " + (i+1) + " é " + temperaturas[i]);
+	       }
+	    }
+
+	}
+
+
