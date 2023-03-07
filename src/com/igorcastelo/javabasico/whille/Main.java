0597e91619dec126c4af24f33eeaebc3bbe111f4
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-06 22:04:18 -0300

    Added another exercise explaining 'Whille' structure

---
+++ src/com/igorcastelo/javabasico/whille/Main.java
@@ -0,0 +1,23 @@
+package com.igorcastelo.javabasico.whille;
+
+public class Main {
+
+	
+	    public static void main(String[] args) {
+	      int i = 1;
+	      int max = 30;
+	      
+	      /*  while(i<max){
+	      System.out.println("O valor de i = " + i);
+	      i++;
+	     
+	      }*/
+	      //do while
+	        do{
+	            i++;
+	            System.out.println("O valor de i = " + i );
+	            }
+	            while(i<max);
+	        }    
+	      
+	    }
