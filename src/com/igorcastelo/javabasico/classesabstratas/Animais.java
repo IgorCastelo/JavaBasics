ce7965bc9d197d18d6606bef6241cb4895fc06c6
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-06 21:35:53 -0300

    Added exercise explaining 'Class Abstract' concept

---
+++ src/com/igorcastelo/javabasico/classesabstratas/Animais.java
@@ -0,0 +1,10 @@
+package com.igorcastelo.javabasico.classesabstratas;
+
+public class Animais {
+	  
+		int idade;
+		String especie;
+		float peso;
+	    
+	    abstract void comer();
+}
---
+++ src/com/igorcastelo/javabasico/classesabstratas/Main.java
@@ -0,0 +1,10 @@
+package com.igorcastelo.javabasico.classesabstratas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
