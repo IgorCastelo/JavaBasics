87d1c94c22c90e1b049910c56269704add9e682a
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-18 01:16:52 -0300

    Foi falado sobre a interface 'LinkedHashSet'  da classe  'Set'.

---
+++ src/com/igorcastelo/javabasico/linkedhashset/Main.java
@@ -0,0 +1,40 @@
+package com.igorcastelo.javabasico.linkedhashset;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> numerosArrayList = new ArrayList<>();
+		numerosArrayList.add(10);
+		numerosArrayList.add(20);
+		numerosArrayList.add(20);
+		numerosArrayList.add(40);
+		numerosArrayList.add(50);
+		numerosArrayList.add(60);
+		
+		System.out.println("ArrayList: " + numerosArrayList);
+		System.out.println(" ");
+		
+		//O HashSet entrega os numeros de forma desoganizada
+		Set<Integer> numerosHashSet = new HashSet<>(numerosArrayList);
+		System.out.println("HashSet: "+ numerosHashSet);
+		System.out.println(" ");
+		
+		//O LinkedHashSet entrega os números organizados de acordo com a posiçãoem que foi inserido, mas sem repetições.
+		LinkedHashSet<Integer> numerosLinkedHashSet = new LinkedHashSet<>(numerosArrayList);
+		System.out.println("LinkedhashSet: "+ numerosLinkedHashSet);
+
+		
+		
+		
+		}
+
+	}
+
+
