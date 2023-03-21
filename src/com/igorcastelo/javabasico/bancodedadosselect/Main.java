339d9dac2449bd91be224cc2ef82cf4abb266504
Igor Castelo <freitascasteloigor@gmail.com>
2023-03-21 18:53:47 -0300

    Foi testado o comando 'Select' no banco de dados pelo Eclipse.

--- src/com/igorcastelo/javabasico/bancodedadosselect/Main.java
+++ src/com/igorcastelo/javabasico/bancodedadosselect/Main.java
@@ -1,31 +1,35 @@
-package com.igorcastelo.javabasico.bancodedadosupdate;
+package com.igorcastelo.javabasico.bancodedadosselect;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
-
 	public static void main(String[] args) {
 		final String usuario = "postgres";
 		final String senha = "Aluno123";
 		final String url = "jdbc:postgresql://localhost:5432/postgres";
-		final String instrucaoUpdate = "UPDATE aluno SET  nome = ? WHERE id=? ";
+		final String instrucaoSelect = "SELECT * FROM professor";
 		final String driverBanco ="org.postgresql.Driver";
 		
 		try {
 			Class.forName(driverBanco);
 			Connection conexao = DriverManager.getConnection(url,usuario,senha);
 			System.out.println("Conexao realizada com sucesso");
-			PreparedStatement  prepareStatement = conexao.prepareStatement(instrucaoUpdate);
+			PreparedStatement  prepareStatement = conexao.prepareStatement(instrucaoSelect);
 			
-			prepareStatement.setString(1, "UPDATE* Igor");
-			prepareStatement.setInt(2,3);
+			ResultSet rs = prepareStatement.executeQuery();
 			
-			prepareStatement.executeUpdate();
-			prepareStatement.executeUpdate();
-			System.out.println("UPDATE realizado com sucesso");
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String nome = rs.getString("nome");
+				System.out.println("Id = " + id + "   Nome: " + nome);
+				
+			}
+			System.out.println("Query(consulta) realizado com sucesso");
+			
 
 		}catch(ClassNotFoundException ex) {
 			System.out.println("Driver não localizado");
@@ -37,4 +41,3 @@ public class Main {
 
 	}
 }
-
